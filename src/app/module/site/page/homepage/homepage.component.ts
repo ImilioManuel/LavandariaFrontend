944a29359509a05e194a2d4f86b7770417f0f4f2
Imílio Manuel <emiliomdmanuel12@gmail.com>
2025-01-27 23:52:27 +0100

    Melhoria nos textos do heropage

--- src/app/module/site/page/homepage/homepage.component.ts
+++ src/app/module/site/page/homepage/homepage.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
+import { BtnComponent } from "../../components/btn/btn.component";
 
 
 @Component({
   selector: 'app-homepage',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, BtnComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })
